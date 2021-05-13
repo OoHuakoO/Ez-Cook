2a465a35ef12bdf4cc5652ee69dd775b86460359
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-05-14 00:58:03 +0700

    add cook page 5%

--- client/lib/pages/add_cook.dart
+++ client/lib/pages/add_cook.dart
@@ -8,6 +8,23 @@ class AddCook extends StatefulWidget {
 class _AddCookState extends State<AddCook> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('เพิ่มสูตรอาหาร'),
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+          child: Column(
+            children: [
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'ชื่อเมนูอาหาร',
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- client/lib/widgets/bottom_nav.dart
+++ client/lib/widgets/bottom_nav.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../pages/add_cook.dart';
 
 class BottomNav extends StatefulWidget {
   @override
@@ -6,18 +7,38 @@ class BottomNav extends StatefulWidget {
 }
 
 class _BottomNavState extends State<BottomNav> {
+  int _currentindex = 0;
+  final List<Widget> _children = [
+    AddCook(),
+    AddCook(),
+    AddCook(),
+    AddCook(),
+    AddCook(),
+  ];
+
+  void onTappedBar(int index) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => _children[index]));
+    setState(() {
+      _currentindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black,
       child: BottomNavigationBar(
+        currentIndex: _currentindex,
+        onTap: onTappedBar,
         backgroundColor: Colors.white,
         selectedItemColor: Colors.red,
         type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
           BottomNavigationBarItem(icon: Icon(Icons.public), label: "world"),
-          BottomNavigationBarItem(icon: Icon(Icons.add_box), label: "add cook"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined), label: "add cook"),
           BottomNavigationBarItem(
               icon: Icon(Icons.leaderboard_rounded), label: "ranking"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "user")
