5ed708c6d81c7b3a79419a3d2b248aea233811ae
huak <komsark_@hotmail.co.th>
2021-05-13 19:18:19 +0700

    servercommit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require("express"),
+  cors = require("cors");
+(userRoutes = require("./routes/user")),
+  (foodRoutes = require("./routes/food")),
+  ({ auth, firestore } = require("./models/index")),
+  (moment = require("moment"));
+moment.locale("th");
+// set up express
+app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+// app
+// Set Route
+app.use("/user", userRoutes);
+app.use("/food", foodRoutes);
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
+  console.log("server start on port 7000");
+});
