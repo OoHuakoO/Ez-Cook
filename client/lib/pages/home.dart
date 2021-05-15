37905e8e15c2093369b79816420fcfd0c56845f5
salongbas1 <thanapon_bas2000@hotmail.com>
2021-05-16 03:32:15 +0700

    home_search

--- client/lib/pages/home.dart
+++ client/lib/pages/home.dart
@@ -8,6 +8,10 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int pageIndex = 0;
+
+  Icon cusIcon = Icon(Icons.search);
+  Widget cusSearchBar = Text("HOME");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,10 +63,53 @@ class _HomeState extends State<Home> {
     if (pageIndex == 0) {
       return AppBar(
         backgroundColor: Color(0xFFF04D56),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(56),
-          child: TextField(),
-        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              right: 5,
+              top: 5,
+              bottom: 10,
+            ),
+            child: IconButton(
+                iconSize: 30,
+                icon: cusIcon,
+                onPressed: () {
+                  setState(() {
+                    if (this.cusIcon.icon == Icons.search) {
+                      this.cusIcon = Icon(Icons.cancel);
+                      this.cusSearchBar = Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: Colors.white),
+                        child: TextField(
+                          style: TextStyle(color: Colors.black),
+                          cursorColor: Colors.white,
+                          cursorHeight: 15,
+                        ),
+                      );
+                    } else {
+                      this.cusIcon = Icon(Icons.search);
+                      this.cusSearchBar = Text("HOME");
+                    }
+                  });
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              right: 5,
+              top: 5,
+              bottom: 10,
+            ),
+            child: IconButton(
+                iconSize: 30,
+                icon: Icon(Icons.notifications),
+                onPressed: () {}),
+          )
+        ],
+        title: cusSearchBar,
       );
     } else if (pageIndex == 1) {
       return AppBar(
