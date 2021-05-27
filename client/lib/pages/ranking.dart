73b0866b847f9ccdeb5ef8c034c0d6e9dc13471f
huak <komsark_@hotmail.co.th>
2021-05-27 12:13:33 +0700

    commit

--- client/lib/pages/ranking.dart
+++ client/lib/pages/ranking.dart
@@ -17,7 +17,7 @@ class _RankState extends State<Rank> {
 
   List<Widget> itemsData = [];
   var foodData;
-  List food;
+  List<Map<String, dynamic>> food = [];
   getRanking() async {
     print("KK");
     try {
@@ -28,10 +28,23 @@ class _RankState extends State<Rank> {
         // print(jsonData);
         // foodData = jsonData.map((e) => new Food.fromJson(e));
         foodData = jsonData.map((e) => new Food.fromJson(e));
-        // for (final listFood in foodData) {
-        //   food.add(listFood.nameFood);
+        // foodData.fromIterable(food, name: (e) => e.nameFood, timeCook: (e) => e.timeCook);
+        // print(food);
+        // typeOf(foodData);
+        for (final listFood in foodData) {
+          food.add({
+            "nameFood": listFood.nameFood,
+            "timeCook": listFood.timeCook,
+            "categoryFood": listFood.categoryFood,
+            "ingredient": listFood.ingredient,
+            "linkYoutube": listFood.linkYoutube,
+            "howCook": listFood.howCook,
+            "imageFood": listFood.imageFood
+          });
+        }
+        // print(food);
         // }
-
+        // print(foodData);
         setState(() {});
       }
     } catch (e) {
@@ -42,119 +55,112 @@ class _RankState extends State<Rank> {
   @override
   void initState() {
     getRanking();
-    // getPostsData();
+    controller.addListener(() {
+      double value = controller.offset / 119;
+      setState(() {
+        topContainer = value;
+        closeTopContainer = controller.offset > 50;
+      });
+    });
     super.initState();
-    // controller.addListener(() {
-    //   double value = controller.offset / 119;
-    //   setState(() {
-    //     topContainer = value;
-    //     closeTopContainer = controller.offset > 50;
-    //   });
-    // });
   }
 
-  void getPostsData() {
-    // List<dynamic> responseList = countFood;
+  getPostsData(index,element) {
+    // List<Map<String, dynamic>> responseList = food;
+    // print(responseList);
     // List<Widget> listItems = [];
-    // responseList.forEach((post) {
-    //   listItems.add(Padding(
-    //     padding: const EdgeInsets.only(top: 10, bottom: 10),
-    //     child: Container(
-    //         height: 80,
-    //         width: 30,
-    //         decoration: BoxDecoration(
-    //           borderRadius: BorderRadius.circular(70),
-    //           color: Color(0xFFFFE6E1),
-    //         ),
-    //         child: Row(
-    //           children: <Widget>[
-    //             ClipRRect(
-    //               borderRadius: BorderRadius.only(
-    //                   topLeft: Radius.circular(80),
-    //                   bottomLeft: Radius.circular(80)),
-    //               child: Container(
-    //                 height: 100,
-    //                 decoration: BoxDecoration(
-    //                   color: Color(0xFFF04D56),
-    //                 ),
-    //                 child: Padding(
-    //                   padding: const EdgeInsets.only(
-    //                       left: 5, right: 5, top: 30, bottom: 5),
-    //                   child: Text(
-    //                     "00",
-    //                     style: const TextStyle(
-    //                         fontSize: 15,
-    //                         fontWeight: FontWeight.bold,
-    //                         color: Colors.white),
-    //                   ),
-    //                 ),
-    //               ),
-    //             ),
-    //             Image.asset(
-    //               "assets/${post["image"]}",
-    //               height: 100,
-    //             ),
-    //             Column(
-    //               children: [
-    //                 Padding(
-    //                   padding: const EdgeInsets.only(left: 5, top: 2),
-    //                   child: Row(
-    //                     children: [
-    //                       Icon(Icons.person),
-    //                       Text(
-    //                         "Sompong",
-    //                       ),
-    //                     ],
-    //                   ),
-    //                 ),
-    //                 Padding(
-    //                   padding: const EdgeInsets.only(top: 2),
-    //                   child: Text(post["name"]),
-    //                 ),
-    //                 Padding(
-    //                   padding: const EdgeInsets.only(left: 20, top: 2),
-    //                   child: Text("ยอดถูกใจทั้งหมด 20 ครั้ง"),
-    //                 )
-    //               ],
-    //             ),
-    //             Column(
-    //               children: [
-    //                 Padding(
-    //                   padding: const EdgeInsets.only(top: 5),
-    //                   child: ClipRRect(
-    //                     borderRadius: BorderRadius.only(
-    //                         topLeft: Radius.circular(50),
-    //                         topRight: Radius.circular(50),
-    //                         bottomLeft: Radius.circular(50),
-    //                         bottomRight: Radius.circular(50)),
-    //                     child: Container(
-    //                       width: 70,
-    //                       height: 30,
-    //                       decoration: BoxDecoration(
-    //                         color: Color(0xFFF04D56),
-    //                       ),
-    //                       child: Padding(
-    //                         padding: const EdgeInsets.only(left: 5, top: 5),
-    //                         child: Text(
-    //                           "อาหารประเภท",
-    //                           style: const TextStyle(
-    //                               fontSize: 9,
-    //                               fontWeight: FontWeight.bold,
-    //                               color: Colors.white),
-    //                         ),
-    //                       ),
-    //                     ),
-    //                   ),
-    //                 ),
-    //               ],
-    //             ),
-    //           ],
-    //         )),
-    //   ));
-    // });
-    // setState(() {
-    //   itemsData = listItems;
-    // });
+    print(element['like']);
+   return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: Container(
+          height: 80,
+          width: 30,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(70),
+            color: Color(0xFFFFE6E1),
+          ),
+          child: Row(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(80),
+                    bottomLeft: Radius.circular(80)),
+                child: Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF04D56),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 5, right: 5, top: 30, bottom: 5),
+                    child: Text(
+                      '${index+1}',
+                      style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              Image.network(element['imageFood']),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5, top: 2),
+                    child: Row(
+                      children: [
+                        Icon(Icons.person),
+                        Text(
+                          "Sompong",
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 2),
+                    child: Text(element['nameFood']),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, top: 2),
+                    child: Text(element['like']),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                          topRight: Radius.circular(50),
+                          bottomLeft: Radius.circular(50),
+                          bottomRight: Radius.circular(50)),
+                      child: Container(
+                        width: 70,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF04D56),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 5, top: 5),
+                          child: Text(
+                            "อาหารประเภท $element['categoryFood']",
+                            style: const TextStyle(
+                                fontSize: 9,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
+    );
   }
 
   @override
@@ -185,10 +191,10 @@ class _RankState extends State<Rank> {
               Expanded(
                   child: ListView.builder(
                       controller: controller,
-                      itemCount: itemsData.length,
+                      itemCount: food.length,
                       physics: BouncingScrollPhysics(),
                       itemBuilder: (context, index) {
-                        return itemsData[index];
+                        return getPostsData(index,food[index]);
                       })),
             ],
           ),
